572b1e4a17a0df8f591eca3e0379dcbdb6c51549
Henri-Willekens <henriwillekens@gmail.com>
2022-09-27 16:18:31 +0200

    login systeem

--- database/migrations/2022_09_20_090336_create_products_table.php
+++ database/migrations/2022_09_20_090336_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->text('description');
             $table->string('image');
-            $table->integer('price');
+            $table->float('price');
             $table->timestamps();
         });
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,17 @@
                 <a class="nav-link active" href="{{ route('home.index') }}">Home</a>
                 <a class="nav-link active" href="{{ route('product.index') }}">Products</a>
                 <a class="nav-link active" href="{{ route('home.about') }}">About</a>
+                <div class="vr bg-white mx-2 d-none d-lg-block"></div>
+                @guest
+                    <a class="nav-link active" href="{{ route('login') }}">Login</a>
+                    <a class="nav-link active" href="{{ route('register') }}">Register</a>
+                @else
+                    <form id="logout" action="{{ route('logout') }}" method="POST">
+                        <a role="button" class="nav-link active"
+                           onclick="document.getElementById('logout').submit();">Logout</a>
+                        @csrf
+                    </form>
+                @endguest
             </div>
         </div>
     </div>
@@ -41,7 +52,7 @@
 <div class="copyright py-4 text-center text-white">
     <div class="container">
         <small>
-             - <b>Henri Willekens</b>
+            - <b>Henri</b>
         </small>
     </div>
 </div>
